d978cd8c0517e6f8dda2790f78c7ce166126e62b
Gerall09 <103030918+Gerall09@users.noreply.github.com>
2022-11-17 22:05:13 +0800

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -14,7 +14,7 @@ import moment from 'moment-timezone'
     surface : 404,
     message: `${ucapan()}`,
     orderTitle: `${ucapan()}`,
-    thumbnail:   await (await fetch(`https://i.ibb.co/jfZVKmC/babi2.jpg`)).buffer(),
+    thumbnail:   await (await fetch(`https://telegra.ph/file/0331c714622f0f984a2e5.jpg`)).buffer(),
     sellerJid: '0@s.whatsapp.net' 
     }
     }
